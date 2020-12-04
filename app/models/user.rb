7ebc947886a098a2fafd86cf4de81969ebc97865
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-04 18:48:37 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   Password_eng_num = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   Password_zen = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
   Password_kana = /\A[ァ-ヶー－]+\z/.freeze
-  validates :encrypted_password, presence: true,format: { with: Password_eng_num}
+  validates :password, presence: true,format: { with: Password_eng_num}
   validates :name, presence: true
   validates :last_name, presence: true,format: {with: Password_zen}
   validates :first_name, presence: true,format: {with: Password_zen}
