11649c8548be2b9dacc605ec4a2c2ddc8d57edc4
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-09 14:09:15 +0900

    itemコントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new,:create]
   def index
   end
 
   def new
   end
   
+  def create
+  @item = Item.create(item_params)
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name,:info,:category,:status,:ship_value,:ship_area,:ship_day,:price,:image).merge(user_id: current_user.id)
+  end
+
 end
