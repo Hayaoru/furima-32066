6b290b348be2bd7c9144f442dfd4785b4a264e9f
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-09 12:52:35 +0900

    itemモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_value
+  belongs_to :ship_area
+  belongs_to :ship_day
 
+  with_options numericality: { other_than: 1 } do
+  validates :category_id ,:status_id, :ship_value_id,:ship_area_id,:ship_day_id 
+  end
+  
+  with_options presence: true do
+    validates :item_name,:info, :image
+  end
+  
+  has_one_attached :image
+  belongs_to :user
+  has_one    :buy_item
   
  
 end
