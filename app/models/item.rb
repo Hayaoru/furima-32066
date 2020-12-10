8c25c0d9122e35a67b0c2c811ad0b73817e1f49a
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-10 12:32:51 +0900

    価格のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
   end
   
   with_options presence: true do
-    validates :item_name,:info, :price, :image
+    validates :item_name,:info, :image
+    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },format: { with: /\A[0-9]+\z/ }
   end
   
   has_one_attached :image
