8a99f9d5e2f9b842ca175a5d8c16d81cb170f6b7
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-12 23:39:00 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+      item_name      {Faker::Lorem.sentence}
+      info           {Faker::Lorem.sentence}
+      category_id    { 3 }
+      status_id      { 2 }
+      ship_value_id  { 2 }
+      ship_area_id   { 2 }
+      ship_day_id    { 2 }
+      price          { 2000 }
+      association :user 
+      
+    end
   end
-end
