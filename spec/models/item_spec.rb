27ac0293159a57a6c121e57599e93634f2b70eba
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-14 12:23:56 +0900

    rubocop実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe Item, type: :model do
     end
 
     it '価格範囲が10,000,000以上だと登録できない' do
-      @item.price = 10000000
+      @item.price = 10_000_000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
