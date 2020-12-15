eb96115510080d39b78c44c8ff3bcd66ea1e4e42
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-15 15:43:11 +0900

    詳細ページ＆編集・削除ボタンの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:item_name, :info, :category_id, :status_id, :ship_value_id, :ship_area_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
   end
