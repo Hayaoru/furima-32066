e021743becdaff19db0da4b0638924ec1902684e
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-16 17:14:41 +0900

    購入画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
   devise_for :users
   root to:"items#index"
   resources :items
   get 'items',to:'items#price'
+  resources :orders, only: :index
 end
