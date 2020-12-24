70234de42468886e1ca659abfb78972cbfa3d2dd
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-24 14:23:12 +0900

    PAYJP実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,9 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+       pay_item
        @order_form.save
        redirect_to root_path
     else
@@ -25,8 +25,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_form).permit(:post_num, :prefecture_id, :city, :address, :building,:phone_num).merge(user_id: current_user.id).merge(item_id: params[:item_id])
+    params.require(:order_form).permit(:post_num, :prefecture_id, :city, :address, :building,:phone_num).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,15 +1,15 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :post_num,:prefecture_id,:city,:address,:building,:phone_num,:item_id,:user_id
+  attr_accessor :post_num,:prefecture_id,:city,:address,:building,:phone_num,:item_id,:user_id,:token
 
   with_options presence: true do
-    validates :prefecture_id,:city,:address,:phone_num
+    validates :token,:city,:address,:phone_num
     validates :post_num, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :phone_num, format: { with: /\A\d{11}\z/}
+    validates :prefecture_id, numericality: { other_than: 1 }
   end
 
   def save
-    binding.pry
     buy_item = BuyItem.create(item_id: item_id,user_id: user_id)
     
     Order.create(post_num: post_num, prefecture_id: prefecture_id,city: city,address: address,building: building,phone_num: phone_num,buy_item_id: buy_item.id)
