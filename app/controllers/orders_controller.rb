721bb3c28653d834ba2298d11ae0a4784ac29e67
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-24 16:01:22 +0900

    ページへのアクセス制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!,only: [:index]
   before_action :set_item,only:[:index,:create]
 
   def index
+    if @item.user == currnet_user || @item.user != current_user && @item.order.present?
+      redirect_to root_path
+    end
     @order_form=OrderForm.new
   end
 
@@ -29,11 +33,11 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
     )
   end
 
