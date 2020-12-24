2a5d0ae20ca5aafb809dab858b6ceb0788b69767
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-12-24 16:11:33 +0900

    訂正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item,only:[:index,:create]
 
   def index
-    if @item.user == current_user || @item.user != current_user && @item.order.present?
+    if @item.user == current_user || @item.user != current_user && @item.buy_item.present?
       redirect_to root_path
     end
     @order_form=OrderForm.new
